9a815509c48fd273981f3ae2d952376b44c7da0a
Daffa06 <kumaraprastya@gmail.com>
2026-02-27 10:16:45 +0700

    power: supply: silence and reduce log driver charging

--- drivers/power/supply/maxim/onewire_gpio.c
+++ drivers/power/supply/maxim/onewire_gpio.c
@@ -21,9 +21,12 @@
 #include <linux/device.h>
 #include <linux/spinlock.h>
 
-#define ow_info pr_err
-#define ow_dbg pr_err
-#define ow_err pr_err
+#undef ow_info
+#undef ow_dbg
+#undef ow_err
+#define ow_info pr_debug
+#define ow_dbg pr_debug
+#define ow_err pr_debug
 #define ow_log pr_err
 
 #define DRV_STRENGTH_16MA (0x7 << 6)
