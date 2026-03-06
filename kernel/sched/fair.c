4bbdd7641c451ade932ef340a69d9c8d32754c0d
Daffa06 <kumaraprastya@gmail.com>
2026-03-06 09:49:17 +0700

    sched: bore: implement Futex Waiting Awareness and Burst Smoothing Update
    This commit adapts advanced optimizations from CachyOS (BORE v5.7.3 and 6.5.5) to the Linux 4.14 baseline for the sm8150 platform.
    
    Key implementation:
    - kernel/futex.c:
      * Implemented when an application is waiting for a system resource (Futex), BORE will mark it with futex_waiting = true.
    - kernel/sched/fair.c:
      * Update adaptations for 4.14 compatibility.
    
    Signed-off-by: Dava<dava@Daffa06>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5517,12 +5517,12 @@ static void dequeue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	int task_sleep = flags & DEQUEUE_SLEEP;
 
 #ifdef CONFIG_SCHED_BORE
-	if (task_sleep) {
-		cfs_rq = cfs_rq_of(se);
-		if (cfs_rq->curr == se)
-			update_curr(cfs_rq);
-		restart_burst(se);
-	}
+    if (task_sleep) {
+        struct cfs_rq *cfs_rq = cfs_rq_of(se);
+        if (cfs_rq->curr == se)
+            update_curr(cfs_rq);
+        restart_burst(se);
+    }
 #endif
 
 	/*
@@ -8791,6 +8791,10 @@ static void check_preempt_wakeup(struct rq *rq, struct task_struct *p, int wake_
 
 	find_matching_se(&se, &pse);
 	update_curr(cfs_rq_of(se));
+#ifdef CONFIG_SCHED_BORE
+    if (likely(sched_bore) && pse->futex_waiting)
+        goto preempt;
+#endif
 	BUG_ON(!pse);
 	if (wakeup_preempt_entity(se, pse) == 1) {
 		/*
