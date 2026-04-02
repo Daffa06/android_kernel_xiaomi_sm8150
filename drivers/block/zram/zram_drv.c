f87974773907641bd1fec9f64a54302c92427bdb
Daffa06 <kumaraprastya@gmail.com>
2026-04-02 17:39:19 +0700

    zram: fix uninitialized ZRAM not releasing backing device commit 74363ec674cb172d8856de25776c8f3103f05e2f upstream.
    Setting backing device is done before ZRAM initialization.  If we set the
    backing device, then remove the ZRAM module without initializing the
    device, the backing device reference will be leaked and the device will be
    hold forever.
    
    Fix this by always reset the ZRAM fully on rmmod or reset store.
    
    Link: https://lkml.kernel.org/r/20241209165717.94215-3-ryncsn@gmail.com
    Fixes: 013bf95a83ec ("zram: add interface to specif backing device")
    Signed-off-by: Kairui Song <kasong@tencent.com>
    Reported-by: Desheng Wu <deshengwu@tencent.com>
    Suggested-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Reviewed-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Dava
    <dava@Daffa06>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -1373,6 +1373,8 @@ static void zram_meta_free(struct zram *zram, u64 disksize)
 	size_t num_pages = disksize >> PAGE_SHIFT;
 	size_t index;
 
+	if (!zram->table)
++		return;
 	/* Free all pages that are still in this zram device */
 	for (index = 0; index < num_pages; index++)
 		zram_free_page(zram, index);
@@ -1380,6 +1382,7 @@ static void zram_meta_free(struct zram *zram, u64 disksize)
 	zs_destroy_pool(zram->mem_pool);
 	zram_dedup_fini(zram);
 	vfree(zram->table);
+	zram->table = NULL;
 }
 
 static bool zram_meta_alloc(struct zram *zram, u64 disksize)
@@ -2215,11 +2218,6 @@ static void zram_reset_device(struct zram *zram)
 	down_write(&zram->init_lock);
 
 	zram->limit_pages = 0;
-
-	if (!init_done(zram)) {
-		up_write(&zram->init_lock);
-		return;
-	}
 
 	set_capacity_and_notify(zram->disk, 0);
 	part_stat_set_all(&zram->disk->part0, 0);
